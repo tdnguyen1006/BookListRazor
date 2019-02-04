ab7a5c5f4d2966223932a6320b32f97ba5f68167
Toan Nguyen <tdnguyen1006@gmail.com>
2019-02-03 22:04:39 -0500

    Add Create New Book Page

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
