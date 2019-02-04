c3d9bd6fbaf29e19656c6d029eb9fb2a5447fd37
Toan Nguyen <tdnguyen1006@gmail.com>
2019-02-03 22:19:39 -0500

    Create Button Post Handler

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -10,11 +10,29 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Books.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
